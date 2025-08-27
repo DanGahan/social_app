3fa6efd7523473cc8cbe543ba14afa0f1e9355e8
Dan Gahan <dangahan@gmail.com>
2025-08-27 12:16:39 +0100

    resolved frontend formatting issues blocking flake8

--- frontend/posts_app/urls.py
+++ frontend/posts_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('users/<int:user_id>/posts/', views.post_list, name='post_list'),
+    path("users/<int:user_id>/posts/", views.post_list, name="post_list"),
 ]
