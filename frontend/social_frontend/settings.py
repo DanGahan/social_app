ebe95c8344ab6c09eb82ad19fad1f903a9608eb6
Dan Gahan <dangahan@gmail.com>
2025-09-18 15:12:41 +0100

    feat: Allow all hosts in Django settings
    This also adds a .gitignore to the frontend to ignore .DS_Store, __pycache__ files and the local db.sqlite3 file.

--- frontend/social_frontend/settings.py
+++ frontend/social_frontend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-c89=g658kx$nigj!x-&r!#+e2+@9)axk0($pnek8xk(71bh8cm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
